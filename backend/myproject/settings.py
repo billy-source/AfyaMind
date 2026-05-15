e0a6eca6a7d7bb7be9ed9b6b544dd14fba13b8bc
Billy Kemboi <kemboibilly49@gmail.com>
2026-05-15 18:46:05 +0300

    Add environment configuration for database, secret key, allowed hosts, and debug mode

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#a1uh2t$50dp66dan4ycm&t2%!6+y4gl*a(thgh@iavwuhzkci'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
